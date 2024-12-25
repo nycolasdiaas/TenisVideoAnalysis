277469d22b91d015aba86b3f8768de36b2d4aacb
nycolasdiaas <nycolaskate2003@hotmail.com>
2024-12-25 15:36:13 -0300

    testando last

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
+model = YOLO('models/last.pt')
 
-result = model.predict('input_videos/input_video.mp4', save=True)
+result = model.track('input_videos/input_video.mp4', conf=0.2,save=True)
 
 print(f'Result:\n {result}')
 print(f'boxes: ')
